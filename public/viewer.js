8f2b7be0d18571b4f6fd40efa4d58b0a6fc8290b
LAMENTIS1 <124012543+LAMENTIS1@users.noreply.github.com>
2024-09-02 13:43:45 +0530

    Update viewer.js

--- public/viewer.js
+++ public/viewer.js
@@ -1,24 +1,22 @@
 window.onload = () => {
     document.getElementById('my-button').onclick = () => {
         init();
-    };
+    }
 }
 
 async function init() {
-    console.log('Init function called');
     const peer = createPeer();
-    peer.addTransceiver('video', { direction: 'recvonly' });
+    peer.addTransceiver("video", { direction: "recvonly" })
 }
 
 function createPeer() {
     const peer = new RTCPeerConnection({
         iceServers: [
             {
-                urls: 'stun:stun.stunprotocol.org'
+                urls: "stun:stun.stunprotocol.org"
             }
         ]
     });
-
     peer.ontrack = handleTrackEvent;
     peer.onnegotiationneeded = () => handleNegotiationNeededEvent(peer);
 
@@ -26,24 +24,18 @@ function createPeer() {
 }
 
 async function handleNegotiationNeededEvent(peer) {
-    console.log('Negotiation needed');
-    try {
-        const offer = await peer.createOffer();
-        await peer.setLocalDescription(offer);
-        const payload = {
-            sdp: peer.localDescription
-        };
+    const offer = await peer.createOffer();
+    await peer.setLocalDescription(offer);
+    const payload = {
+        sdp: peer.localDescription
+    };
 
-        const { data } = await axios.post('/consumer', payload);
-        const desc = new RTCSessionDescription(data.sdp);
-        await peer.setRemoteDescription(desc);
-    } catch (error) {
-        console.error('Error during negotiation:', error);
-    }
+    const { data } = await axios.post('/consumer', payload);
+    const desc = new RTCSessionDescription(data.sdp);
+    peer.setRemoteDescription(desc).catch(e => console.log(e));
 }
 
-function handleTrackEvent(event) {
-    console.log('Track event received');
-    const videoElement = document.getElementById('video');
-    videoElement.srcObject = event.streams[0];
-}
+function handleTrackEvent(e) {
+    document.getElementById("video").srcObject = e.streams[0];
+};
+
